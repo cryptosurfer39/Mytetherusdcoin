b7d0cefe788a1de2382cd438cee7f6cca5ceeb0d
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-02 15:29:02 -0800

    Update index.tsx

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,11 +1,11 @@
-import { USDT } from "react"
+import { fc } from "react"
 import Image from "next/image"
 
 type LogoProps = {
   size?: string
 }
 
-export const Logo: USDT<LogoProps> = ({ size = "text-lg" }) => {
+export const Logo: FC<LogoProps> = ({ size = "text-lg" }) => {
   return (
     <div className={`flex items-center ${size} gap-1 font-audiowide tracking-wide`}>
       <Image src="/images/TetherUSDLogo.svg" alt="tether" width={34} height={30} />
