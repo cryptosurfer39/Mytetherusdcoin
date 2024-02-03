a956cc4807840b4ab706ae4845fcd471af2a2874
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-02 17:38:05 -0800

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,24 +1,24 @@
-import { CHAIN_ID, RINKEBY_CHAIN_ID } from "config"
+import { CHAIN_ID, BSC_CHAIN_ID } from "config";
 import { AddEthereumChainParameter } from "@3rdweb/hooks"
 
 // Chains & Icons -> https://github.com/ethereum-lists/chains/tree/master/_data
 
 interface t {
-  [key: number]: AddEthereumChainParameter
+  [key: number]: AddEthereumChainParameter;
 }
 
 export const addNetowrkMetadata: t = {
-  [RINKEBY_CHAIN_ID]: {
-    chainId: `0x${CHAIN_ID.toString(16)}`,
-    chainName: "Rinkeby",
+  [BSC_CHAIN_ID]: {
+    chainId: `0x${BSC_CHAIN_ID.toString(16)}`,
+    chainName: "Binance Smart Chain",
     nativeCurrency: {
-      name: "Rinkeby Ether",
-      symbol: "ETH",
-      decimals: 18
+      name: "Binance Coin",
+      symbol: "BNB",
+      decimals: 18,
     },
-    rpcUrls: ["https://rinkeby.infura.io/v3/"],
-    blockExplorerUrls: ["https://rinkeby.etherscan.io"],
-    iconUrls: [""]
+    rpcUrls: ["https://bsc-dataseed.binance.org/"],
+    blockExplorerUrls: ["https://bscscan.com/"],
+    iconUrls: [""], // Add actual icon URLs for BSC
   }
 }
 
