f09f037f1622180ac7e1c5cd060ae0125e48f180
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-02 22:51:16 -0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,15 +9,15 @@ export default function Document () {
         <meta name="description" content="Tether USD Coin is a Community created for Stablecoin to be pegged"/>
 
         <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://foxcoin.vercel.app/"/>
-        <meta property="og:title" content="FoxCoin"/>
-        <meta property="og:description" content="Fox Coin is a dapp created for ethereum 101 course of cadena.dev"/>
+        <meta property="og:url" content="https://mytetherusdcoin.vercel.app/"/>
+        <meta property="og:title" content="TetherUSD"/>
+        <meta property="og:description" content="Tether USD Coin is a Community created for Stablecoin to be pegged"/>
         <meta property="og:image" content="https://foxcoin.vercel.app/assets/LandingPage.png"/>
 
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://foxcoin.vercel.app/"/>
         <meta property="twitter:title" content="FoxCoin"/>
-        <meta property="twitter:description" content="Fox Coin is a dapp created for ethereum 101 course of cadena.dev"/>
+        <meta property="twitter:description" content="Tether USD Coin is a Community created for Stablecoin to be pegged"/>
         <meta property="twitter:image" content="https://foxcoin.vercel.app/assets/LandingPage.png"/>
       </Head>
       <body>
