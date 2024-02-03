6288f69315972856ac798019fcbff255e51ceed3
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-02 17:13:08 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
         <div className="relative grid h-full grid-cols-2 gap-10 p-4 pt-40 pb-32 my-auto overflow-hidden xl:px-8 xs:grid-cols-1">
           <div className="z-10 flex flex-col gap-12">
             <div className="flex flex-col gap-6">
-              <div className="flex items-center gap-2 px-4 border-2 border-orange-500 rounded-full text-neutral-200 max-w-max">
+              <div className="flex items-center gap-2 px-4 border-2 border-green-500 rounded-full text-neutral-200 max-w-max">
                 <div className="w-2 h-2 bg-green-500 rounded-full"></div>
                 <span className="text-sm font-semibold">Live on BNBChain</span>
               </div>
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
                 <span className="text-black">Little meme.</span>
                 <span className="text-black">Big dream.</span>
               </h1>
-              <p className="text-gray-300">Explore the crypto world. Join Community of Stablecoin</p>
+              <p className="text-goldenrod-300">Explore the crypto world. Join Community of Stablecoin</p>
             </div>
             <button className="flex items-center gap-2 p-4 px-6 transition-all bg-green-500 max-w-max hover:scale-105" onClick={handleClaimAirdrop}>
               <span className="font-semibold">Get Airdrop</span>
