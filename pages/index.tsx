411e258cc2f44eed266a4013a8860f033cea92cb
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-02 16:14:03 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,23 +16,23 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>FoxCoin</title>
+        <title>TetherUSD</title>
       </Head>
       <div className="flex flex-col gap-16">
         <div className="relative grid h-full grid-cols-2 gap-10 p-4 pt-40 pb-32 my-auto overflow-hidden xl:px-8 xs:grid-cols-1">
           <div className="z-10 flex flex-col gap-12">
             <div className="flex flex-col gap-6">
               <div className="flex items-center gap-2 px-4 border-2 border-orange-500 rounded-full text-neutral-200 max-w-max">
-                <div className="w-2 h-2 bg-orange-500 rounded-full"></div>
-                <span className="text-sm font-semibold">Live on Rinkeby</span>
+                <div className="w-2 h-2 bg-green-500 rounded-full"></div>
+                <span className="text-sm font-semibold">Live on BNBChain</span>
               </div>
               <h1 className="flex flex-col gap-6 font-semibold text-7xl sm:text-6xl xs:text-6xl">
-                <span className="text-white">Little meme.</span>
-                <span className="text-white">Big dream.</span>
+                <span className="text-black">Little meme.</span>
+                <span className="text-black">Big dream.</span>
               </h1>
-              <p className="text-gray-300">Explore the crypto world. Join Community of Fox</p>
+              <p className="text-gray-300">Explore the crypto world. Join Community of Stablecoin</p>
             </div>
-            <button className="flex items-center gap-2 p-4 px-6 transition-all bg-orange-500 max-w-max hover:scale-105" onClick={handleClaimAirdrop}>
+            <button className="flex items-center gap-2 p-4 px-6 transition-all bg-green-500 max-w-max hover:scale-105" onClick={handleClaimAirdrop}>
               <span className="font-semibold">Get Airdrop</span>
               { claimAirdropMutation.isLoading && <AiOutlineLoading3Quarters className="animate-spin" size="1.2rem" /> }
               { !claimAirdropMutation.isLoading && <BsArrowUpRight /> }
